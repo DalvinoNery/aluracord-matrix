28077b9dfbfcd1164e57e4430cd75d17e5f478ec
DalvinoNery <rodrigo.s.nery@gmail.com>
2022-01-26 21:16:29 -0300

    Aula 02 e 03

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import {useRouter} from 'next/router'
 import appConfig from "../config.json"
 
-function GlobalStyle(){
-    return (
-        <style global jsx>{`
-          * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            list-style: none;
-          }
-          body {
-            font-family: 'Open Sans', sans-serif;
-          }
-          /* App fit Height */ 
-          html, body, #__next {
-            min-height: 100vh;
-            display: flex;
-            flex: 1;
-          }
-          #__next {
-            flex: 1;
-          }
-          #__next > * {
-            flex: 1;
-          }
-          /* ./App fit Height */ 
-        `}</style>
-      );
-}
+
 
 function Titulo(props){
     const Tag = props.tag;
@@ -61,16 +36,17 @@ function Titulo(props){
 // export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'peas';
+    //const username = 'DalvinoNery';
+    const [username, setUsername] = React.useState('')
+    const roteamento = useRouter();
   
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
             backgroundColor: appConfig.theme.colors.primary[500],
-            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/12/apollo-mission-operations-control-room.jpg)',
             backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
           }}
         >
@@ -92,6 +68,11 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (infosDoEvento){
+                infosDoEvento.preventDefault();
+                roteamento.push('/chat');
+
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -102,7 +83,13 @@ export default function PaginaInicial() {
                 {appConfig.name}
               </Text>
   
-              <TextField
+               <TextField
+               value={username}
+               onChange={function (event){
+                 const valor = event.target.value;
+                 setUsername(valor);
+ 
+               }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
@@ -111,7 +98,7 @@ export default function PaginaInicial() {
                     mainColorHighlight: appConfig.theme.colors.primary[500],
                     backgroundColor: appConfig.theme.colors.neutrals[800],
                   },
-                }}
+                }} 
               />
               <Button
                 type='submit'
